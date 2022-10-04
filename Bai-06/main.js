26734af9636e0c0be1c7a9f76c61ce6ffff06b96
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-04 21:55:41 +0700

    add Bai-06

---
+++ Bai-06/main.js
@@ -0,0 +1,24 @@
+/** 
+ * Comments trong js
+ * 
+ * 1. Mục đích sử dụng
+ * - Ghi chú
+ * - Vô hiệu hoá mã
+ * 
+ * 2. Loại comments
+ * - Coments một dòng
+ * - Coments nhiều dòng
+ * 
+ * 3. Sử dụng phím tắt 
+ * - Windows: Ctrl + /
+ * - MacOS: Command + /
+ */
+
+// Khai báo biến
+
+var fullName = 'Hà Ngọc Đương'
+var age = 24
+
+// Gọi hàm thông báo
+alert(fullName)
+alert(age)
