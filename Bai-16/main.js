1abcc0d4f26e22c0133c73954ad22cfa35cfae2c
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-05 21:47:22 +0700

    add Bai-16

---
+++ Bai-16/main.js
@@ -0,0 +1,18 @@
+/** BAI-16
+ * 
+ * Toán tử logic
+ * 
+ *  1. && - And
+ *  2. || - Or
+ *  3. ! - Not
+ */
+
+
+var a = 1
+var b = 2
+var c = 3
+
+if (a > 0 && b > 0) {
+    console.assert.log('DIEU KIEN DUNG')
+}
+
