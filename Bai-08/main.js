394726a04b6ed7c52ac791ba9555b5a5cdc533dc
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-05 16:22:56 +0700

    add Bai-08

---
+++ Bai-08/main.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Giới thiệu về toán tử trong Javascript
+ * 1. Toán tủ số học - Airthmetic
+ * 2. Toán tử gán - Assignment
+ * 3. Toán tử so sánh - Comparison
+ * 4. Toán tử logic - Logiccal
+ * 
+ */
+var a = 1 * 2;
+console.log(a)
+
+var a = 1
+var b = 2
+
+if (a > 0 && b > 0) {
+    alert('a & b lon hon 0')
+}
