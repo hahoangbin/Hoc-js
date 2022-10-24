27fa1035c19bd95e95d729fe7a1bd91ca4769b85
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-24 18:17:58 +0700

    add Bai-23

---
+++ Bai-23/main.js
@@ -0,0 +1,22 @@
+/*
+Mảng trong javarscript - array
+
+1. Tạo mảng
+    - Cách tạo 
+    - Sử dụng cách nào? Tại sao?
+    - Kiểm tra data type
+
+2. Truy xuất mảng
+    - Độ dài mảng
+    - Lấy phần tử theo index
+*/
+
+var languages = [
+    'Javarscript',
+    'PHP',
+    'Ruby',
+    'Dart'
+]
+
+// console.log(languages)
+console.log(languages[2])
