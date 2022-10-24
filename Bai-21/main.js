35b954e82df1eb979c546272b4c66f8ceb777ad2
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-24 17:01:00 +0700

    add Bai-21

---
+++ Bai-21/main.js
@@ -0,0 +1,35 @@
+
+// LÀM VIỆC VỚI CHUỖI
+
+var myString = 'Hoc JS tai F8'
+
+// Keyword: Javarcript string methods
+
+// 1 Length
+// console.log(myString.length)
+
+// 2. Find index
+// console.log(myString.indexOf('JS'))
+
+// 3. Cut string    
+// console.log(myString.slice(4, 6))
+
+// 4. Replace
+// console.log(myString.replace(/JS/g, 'Javarscript'))
+
+// 5. Convert to upper case 
+// console.log(myString.toUpperCase())
+
+// 6. Convert to lower case
+// console.log(myString.toLowerCase())
+
+// 7. Trim
+// console.log(myString.trim().length)
+
+// 8. Split
+// var languaes = 'Javarcript, PHP, Ruby'
+// console.log(languaes.split(', '))
+
+// 9. Get a character by index
+const myString2 = 'Son Dang'
+console.log(myString2[10])
