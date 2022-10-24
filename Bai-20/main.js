be6b804d0acd878360c3523fb2a3eefa06138534
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-24 12:01:34 +0700

    add Bai-20

---
+++ Bai-20/main.js
@@ -0,0 +1,17 @@
+/*
+CHUỖI TRONG JAVASCRIPT
+
+1. Tạo chuỗi
+    - Các cách tạo chuỗi
+    - Nên dùng cách nào? lý do?
+    - Kiểm tra data type
+2. Một số case sử dụng backslash (\)
+3. Xem độ dài chuỗi
+4. Chú ý độ dài khi viết Code
+5. Template string ES6
+*/
+
+var firstName = 'Son'
+var lastName = 'Dang'
+
+console.log(`Toi la: ${firstName} ${lastName}`)
