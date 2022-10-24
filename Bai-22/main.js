e5ff7f7f5fad33ec8abd9112b3a31fcae30e7ef9
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-24 17:28:44 +0700

    add Bai-22

---
+++ Bai-22/main.js
@@ -0,0 +1,17 @@
+/*
+Kiểu số (Number) trong javarscript
+
+1. Tạo giá trị Number
+    - Các cách tạo 
+    - Dùng cách nào? Tại sao?
+    - Kiểm tra data type
+
+2. Làm việc với Number
+    - To string
+    - To fixed
+*/
+
+var age = 18
+var PI = 3000.2465783404
+
+console.log(PI.toFixed(3))
