91bbfdbfc0df62160cc891406068e8e557507ae2
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-24 10:25:56 +0700

    add Bai-17

---
+++ Bai-17/main.js
@@ -0,0 +1,27 @@
+/*  BAI-17
+
+Kiểu dữ liệu trong Javascript
+
+1. Dữ liệu nguyên thủy - Primitive Data
+    - Number
+    - String
+    - Boolean
+    - Undefined
+    - Null
+    - Symbol
+
+2. Dữ liệu phức tạp - Complex Data
+    - Fuction
+    - Object
+    */
+
+// Number type
+    var a = 1
+    var b = 2 
+    var c = 1.5
+
+// String type
+    fullName = 'Son Dang';
+
+// Boolean type
+
