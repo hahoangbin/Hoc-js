b5f330db98f146c6d81e85bbe6336ab76832bfd7
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-25 11:22:15 +0700

    add Bai-25

---
+++ Bai-25/main.js
@@ -0,0 +1,25 @@
+/*
+Hàm (function) trong javarscript
+
+1. Hàm?
+    - Một khối mã
+    - Làm một việc cụ thể
+
+2. Loại hàm
+    - Built-in
+    - Tự định nghĩa
+
+3. Tính chất
+    - Không thực thi khi định nghĩa
+    - Sẽ thực thi khi được gọi
+    - Có thể nhận tham số 
+    - Có thể trả vể một giá trị
+
+4. Tạo hàm đầu tiên
+*/
+
+function showDialog() {
+    alert('Hi, xin chao cac ban!')
+}
+
+showDialog();
