1a2798af8928d482d59ef95b218b7af5d2aacfe6
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-25 21:37:10 +0700

    add Bai-36-37

---
+++ Bai-37/main.js
@@ -0,0 +1,13 @@
+
+// Toán tử 3 ngôi - Ternary operator
+
+var course = {
+    name: 'Javascript',
+    coin: 250
+}
+
+if (course.coin > 0) {
+    console.log(`${course.coin} Coins`)
+} else {
+    console.log('Miễn phí')
+}
