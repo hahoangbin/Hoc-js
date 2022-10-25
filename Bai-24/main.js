d939cec503fa0a5ef6a72aaf0af853d5e40acb0b
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-25 10:51:50 +0700

    add Bai-24

---
+++ Bai-24/main.js
@@ -0,0 +1,50 @@
+/*
+Làm  việc với Array
+
+// Keyword: Javarcript array methods
+
+1. To string
+2. Join
+3. Pop
+4. Push
+5. Shift
+6. Unshift
+7. Splicing
+8. Concat
+9. Slicing
+*/
+
+var languages = [
+    'Javarscript',
+    'PHP',
+    'Ruby',
+]
+
+// console.log(languages.join(', '))
+
+// console.log(languages.pop())
+// console.log(languages.pop())
+// console.log(languages.pop()) xoá phần tử cuối mảng
+
+// console.log(languages.shitf())
+// console.log(languages.shitf())
+// console.log(languages.shitf()) xoá phần tử đầu mảng
+
+
+// console.log(languages.push('Dart', 'Java')) Thêm phần tử vào cuối mảng
+
+// console.log(languages.unshift('Dart', 'Java')) Thêm phần tử vào đầu mảng
+
+// languages.splice(1, 0, 'Dart') xoá phần tử hoặc thêm phần tử
+
+var languages2 = [
+    'Dart',
+    'Java'
+]
+
+// console.log(languages.concat(languages2)) nối hai mảng với nhau
+
+// console.log(languages.slice(2)) cắt mảng
+
+
+
