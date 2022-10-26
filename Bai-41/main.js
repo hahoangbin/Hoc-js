f4e0e29c663de5e23868206e8f430cd7322eb16b
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-26 16:09:03 +0700

    add Bai-38-39-40-41

---
+++ Bai-41/main.js
@@ -0,0 +1,46 @@
+
+/*  Bai-38
+
+    Vòng lặp - Loop
+
+    1. for - Lặp với điều kiện đúng
+    2. for/in - Lặp qua key của đối tượng
+    3. for/of - Lặp qua value của đối tượng
+    4. while - Lặp khi điều kiện đúng
+    5. do/while - Lặp ít nhất 1 lần, sau đó lặp khi điều kiện đúng
+*/
+
+/*  Bai-39
+
+    For loop
+// */
+// for (var i = 1; i <= 1000; i++) {
+//     console.log(i)
+// }
+
+/*  Bai-40
+
+    For loop
+// */
+// for (var i = 1; i <= 1000; i++) {
+//     console.log(i)
+// }
+
+// Bai-41
+
+// For loop
+
+var myArray = [
+    'javascript',
+    'PHP',
+    'Ruby',
+    'Java',
+    'avatar',
+    'Temo'
+]
+var arrayLength = myArray.length
+
+for (var i = 0; i < arrayLength; i++) {
+    console.log(myArray[i])
+}
+
