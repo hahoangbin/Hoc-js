610e61c2cd5d78389f39d103496959d9636338f2
Ngoc Duong <hahoangbin999@gmail.com>
2022-10-26 21:49:49 +0700

    add Bai-46-48

---
+++ Bai-48/main.js
@@ -0,0 +1,34 @@
+// Bai-46
+
+// Break & Continue in loop
+
+// for (var i = 0; i < 10; i++) {
+    
+//     if (i%2 !== 0) {
+//         continue
+//     }
+//     console.log(i)
+// }
+
+// Bai-47
+
+// Vòng lặp lồng nhau nested loop
+
+// var myArray = [
+//     [1, 2],
+//     [3, 4],
+//     [5, 6]
+// ]
+// for ( var i = 0; i < myArray.length; i++) {
+//     for (var j = 0; j < myArray[i].length; j++) {
+//         console.log(myArray[i][j])
+//     }
+// }
+
+// Bai-48
+
+// Loop...
+
+for (i = 0; i <= 100; i += 5) {
+    console.log(i)
+}
