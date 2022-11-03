1ddcefa2d63063ad8444f168b41259f1ec74a36a
Ngoc Duong <hahoangbin999@gmail.com>
2022-11-03 11:01:16 +0700

    add bai 66-67

---
+++ Bai-66/main.js
@@ -0,0 +1,43 @@
+// Bai-66
+
+// HTML DOM
+// Document Objeck Mode
+
+// 3 thanh phan
+
+// 1. Element: ID, class, tag, CSS seletor, HTML collection
+// 2. Attribute
+// 3. Text
+
+// var headingNode = document.getElementById('heading')
+
+// console.log(headingNode)
+
+// var headingNodes = document.getElementsByClassName('heading')
+
+// console.log(headingNodes)
+
+// var headingNodes = document.getElementsByTagName('p')
+
+// console.log(headingNodes)
+
+// var headingNodes = document.querySelectorAll('.heading')
+
+// console.log(headingNodes[1])
+
+
+// Bai 67
+
+// HTML DOM
+
+// 1. Element: ID, class, tag, CSS seletor, HTML collection
+// 2. Attribute
+// 3. Text
+
+var boxNode = document.querySelectorAll('.box-1')
+console.log(boxNode)
+
+console.log(boxNode.getElementsByTagName('li'))
+
+console.log(boxNode.querySelector('p'))
+
