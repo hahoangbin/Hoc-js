478d9c7d4db1d800fde66185c266689d3288fab8
Ngoc Duong <hahoangbin999@gmail.com>
2022-11-03 17:26:42 +0700

    add Bai-72

---
+++ Bai-72/main.js
@@ -0,0 +1,15 @@
+
+// Bai 72
+
+// 1. Element
+// 2. Attribute
+// 3. Text
+
+//  innerHTML, outerHTMLL
+
+var boxElement = document.querySelector('.box')
+
+
+boxElement.outerHTML = '<h1>Heading</h1>'
+
+console.log(boxElement)
