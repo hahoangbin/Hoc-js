0f4eb7a37093e37ca40c211b7e535ece645dc6ab
Ngoc Duong <hahoangbin999@gmail.com>
2022-11-08 11:03:33 +0700

    add Bai-87-88

---
+++ Bai-87/app.js
@@ -0,0 +1,18 @@
+/*
+    1. Event listener --> OK
+    2. JSON
+    3. Fetch
+    4. DOM Location
+    5. Local storage
+    6. Session torage
+    7. Coding convention
+    8. Best Practices
+    9. Mistakes
+    10. Performance
+*/
+// JSON Là một định dạng giữ liệu (chuỗi) JavaScript Object Notation
+// JSON: Thể hiện dữ liệu dạng number, boolean, null, object, array
+
+// thao tác chuyển đổi
+// Stringify / Parse
+// mã hoá <=> giải mã (hai chiều)
---
+++ Bai-88/app.js
@@ -0,0 +1,23 @@
+/*
+    1. Event listener --> OK
+    2. JSON --> OK
+    3. Promise - Sync
+               - Async
+               - Nỗi đau
+               - Lý thuyết, cách hoạt động 
+               - Thực hành, ví dụ
+    4. Fetch
+    5. DOM Location
+    6. Local storage
+    7. Session torage
+    8. Coding convention
+    9. Best Practices
+    10. Mistakes
+    11. Performance
+*/
+// JSON Là một định dạng giữ liệu (chuỗi) JavaScript Object Notation
+// JSON: Thể hiện dữ liệu dạng number, boolean, null, object, array
+
+// thao tác chuyển đổi
+// Stringify / Parse
+// mã hoá <=> giải mã (hai chiều)
