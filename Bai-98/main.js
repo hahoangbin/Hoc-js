b04c96d15b3d4c729b73b0d6c2b751bb6105d4a4
Ngoc Duong <hahoangbin999@gmail.com>
2022-11-10 16:31:25 +0700

    add Bai-99-101

--- Bai-98/main.js
+++ Bai-98/main.js
@@ -4,11 +4,11 @@
     3. Multi-line string --> OK
     4. Arrow function đặt--> OK
     5. Classes --> OK
-    6. Enhanced object literals
-    7. Default parameter values
-    8. Destructuring
-    9. Rest parameter
-    10. Spread
+    6. Enhanced object literals --> OK
+    7. Default parameter values --> OK
+    8. Destructuring --> OK
+    9. Rest parameter --> OK
+    10. Spread --> OK
     11. Tagged template litera
     12. Modules
 */
@@ -28,14 +28,9 @@
 //     }
 // }
 // console.log(course.getName())
+var array1 = ['Javascript', 'PHP', 'Ruby']
+var array2 = ['ReactJS', 'Dart']
+var array3 = ['Abc', 'Center', 'TOT']
+var array4 = [[...array1], [...array2], [...array3]]
 
-var fiedName = 'name'
-var fiedPrice = 'price'
-
-const course = {
-    name: 'Javascript',
-    [fiedName]: 'PHP',
-    [fiedPrice]: 1000
-}
-
-console.log(course)
+console.log(array4)
