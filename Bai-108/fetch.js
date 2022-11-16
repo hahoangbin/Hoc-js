09fed5693d19ca861501fe35838c47ab53d5dbb5
Ngoc Duong <hahoangbin999@gmail.com>
2022-11-16 13:14:21 +0700

    add Bai-108

---
+++ Bai-108/fetch.js
@@ -0,0 +1,43 @@
+// BAI 108
+
+/*
+    1. Event listener --> OK
+    2. JSON --> OK
+            - Ứng dụng trong thực tế của JSON?
+    3. Promise - Sync ---> OK
+               - Async ---> OK
+               - Nỗi đau ---> OK
+               - Lý thuyết, cách hoạt động --> OK
+               - Thực hành, ví dụ --> OK
+               - Ứng dụng thực tế của Promise? --> OK
+    4. Fetch
+               - JSON server: API server (Face)
+               - CRUD -> POST
+                      -> GET
+                      -> PUT
+                      -> PATCH
+                      -> DELETE
+
+                - Postman 
+    5. DOM Location
+    6. Local storage
+    7. Session torage
+    8. Coding convention
+    9. Best Practices
+    10. Mistakes
+    11. Performance
+*/
+
+
+// var courseApi = 'http://localhost:3000/courses'
+
+// fetch(courseApi)
+//    .then(function(reponse) {
+//         return reponse.json()
+//    })
+//    .then(function(courses) {
+//         console.log(courses)
+//    })
+ 
+
+// BAI 109
